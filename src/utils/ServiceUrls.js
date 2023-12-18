1d33dbddbf22c42bbe6f25f2fbf7ec9932126e96
ahmadSakib <sakib@divine-it.net>
2023-12-18 12:44:20 +0600

    all removed

--- src/utils/ServiceUrls.js
+++
@@ -1,3 +0,0 @@
-export const MASTER_DATA_BASE_URL = "http://localhost:8960/api/masterData";
-export const LOCATION_DISTRICT_URL = "/Locations/List/";
-export const LOCATION_UPAZILA_URL = "/Locations/List/";
